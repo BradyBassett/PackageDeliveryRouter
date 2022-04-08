64a4b1f6fab8ab0dffa81f4393e22e4d09599e4c
BradyBassett <bradydbassett@gmail.com>
2022-04-07 21:42:55 -0500

    typing

--- src/models/node.py
+++ src/models/node.py
@@ -4,17 +4,16 @@ if TYPE_CHECKING:
 
 
 class Node:
-    def __init__(self, node_id: int, node_name: str, node_address: str, node_zipcode: str):
+    def __init__(self, node_id: int, node_name: str, node_address: str, node_zipcode: str) -> None:
         self.node_id: int = node_id
         self.node_name: str = node_name
         self.node_address: str = node_address
         self.node_zipcode: str = node_zipcode
         self.edges: list["Edge"] = []
-        self.weight: float = 0
 
     def __repr__(self) -> str:
         return f"ID: {self.node_id}, Name: {self.node_name}, Address: {self.node_address}, ZipCode: " \
                f"{self.node_zipcode},\nEdge Ids: {self.edges}"
 
-    def add_edge(self, edge: "Edge"):
+    def add_edge(self, edge: "Edge") -> None:
         self.edges.append(edge)
