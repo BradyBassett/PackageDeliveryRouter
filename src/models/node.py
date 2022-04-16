93921467c06d42f68b92e022a5c8aa2d1f747dc8
BradyBassett <bradydbassett@gmail.com>
2022-04-16 14:16:22 -0500

    comparison operations

--- src/models/node.py
+++ src/models/node.py
@@ -12,11 +12,20 @@ class Node:
         self.node_zipcode: str = node_zipcode
         self.edges: list["Edge"] = []
         self.cost: float = sys.maxsize
-        self.prev_node: list["Node"] = []
+        self.prev_node: list[Node] = []
 
     def __repr__(self) -> str:
         return f"ID: {self.node_id}, Name: {self.node_name}, Address: {self.node_address}, ZipCode: " \
                f"{self.node_zipcode},\nEdge Ids: {self.edges}"
+
+    def __hash__(self):
+        return id(self)
+
+    def __eq__(self, other: "Node"):
+        return self.node_id == other.node_id
+
+    def __lt__(self, other: "Node"):
+        return self.cost < other.cost
 
     def add_edge(self, edge: "Edge") -> None:
         self.edges.append(edge)
