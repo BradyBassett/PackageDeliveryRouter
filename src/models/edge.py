f2088680f702af7151d63e2584b95bb213fda6e0
BradyBassett <bradydbassett@gmail.com>
2022-04-16 14:17:42 -0500

    comparison methods

--- src/models/edge.py
+++ src/models/edge.py
@@ -6,15 +6,21 @@ if TYPE_CHECKING:
 
 class Edge:
     def __init__(self, node_1: "Node", node_2: "Node", distance: float) -> None:
-        self.node_1: "Node" = node_1
+        self.node_1: Node = node_1
         node_1.add_edge(self)
-        self.node_2: "Node" = node_2
+        self.node_2: Node = node_2
         node_2.add_edge(self)
         self.distance: float = distance
         self.priority: float = 0
 
     def __repr__(self) -> str:
         return f"{self.distance} miles between {self.node_1.node_name} and {self.node_2.node_name}"
+
+    def __hash__(self):
+        return id(self)
+
+    def __eq__(self, other: "Edge"):
+        return self.node_1.node_id == other.node_1.node_id and self.node_2.node_id == other.node_2.node_id
 
     def eligible(self, nodes: list["Node"]) -> bool:
         # Intended to check a group of nodes and see if any of them are present within the current edge to filter
