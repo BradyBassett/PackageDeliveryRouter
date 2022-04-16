78b3723b6ae1d97a82dcc3fc9753cbd908e2d774
BradyBassett <bradydbassett@gmail.com>
2022-04-15 20:36:01 -0500

    filter_truck_graph method

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ from models.hash_table import HashTable
 from models.graph import Graph
 from models.edge import Edge
 from models.node import Node
-from models.heap import Heap
 from models.truck import Truck
 from models.driver import Driver
 
@@ -21,7 +20,6 @@ class Application:
     def __init__(self) -> None:
         self.packages: "HashTable" = HashTable()
         self.graph: "Graph" = Graph()
-        self.heap: "Heap" = Heap()
         self.trucks: list["Truck"] = [Truck(i + 1) for i in range(NUMBER_OF_TRUCKS)]
         self.drivers: list["Driver"] = [Driver(i + 1) for i in range(NUMBER_OF_DRIVERS)]
 
@@ -29,6 +27,8 @@ class Application:
         self.load_packages()
         self.load_trucks()
         self.load_distances()
+        for i in range(len(self.trucks)):
+            self.trucks[i].delivery_graph = self.filter_truck_graph(i)
 
     def load_packages(self) -> None:
         with open("data/packages.csv") as file:
@@ -59,14 +59,19 @@ class Application:
                     if i is not j:
                         node_1: "Node" = self.graph.nodes[i]
                         node_2: "Node" = self.graph.nodes[j]
-                        self.graph.add_edge(Edge(node_1, node_2, row[j]))
+                        self.graph.add_edge(Edge(node_1, node_2, float(row[j])))
 
-    def record_deliveries(self, index: int) -> None:
+    def filter_truck_graph(self, index: int) -> "Graph":
         package_addresses: list[str] = [p.address for p in self.trucks[index].packages]
-        package_nodes: list["Node"] = [n for n in self.graph.nodes if n.node_address in package_addresses]
-        edges_filter: filter = filter(lambda x: x.eligible(package_nodes), self.graph.edges)
-        # TODO - get a sorted list of nodes to deliver to based on some weight value
-        # sorted_edges: list["Edge"] = sorted(edges_filter, key=lambda x: x.distance)
+        package_nodes: list["Node"] = [n for n in self.graph.nodes if n.node_address in package_addresses
+                                       or n.node_address == "HUB"]
+        filtered_edges: filter = filter(lambda x: x.eligible(package_nodes), self.graph.edges)
+
+        truck_graph: "Graph" = Graph(package_nodes, filtered_edges)
+        for edge in truck_graph.edges:
+            edge.calculate_priority(self.trucks[index].packages)
+
+        return truck_graph
 
 
 def main() -> None:
