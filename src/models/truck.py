7b3cfae8a53eff612c431a005df188a8900e83df
BradyBassett <bradydbassett@gmail.com>
2022-04-16 14:15:42 -0500

    implementing dijkstras algorithm

--- src/models/truck.py
+++ src/models/truck.py
@@ -16,13 +16,13 @@ AVG_SPEED: int = 18
 class Truck:
     def __init__(self, truck_id: int) -> None:
         self.truck_id: int = truck_id
-        self.driver: Optional["Driver"] = None
+        self.driver: Optional[Driver] = None
         self.capacity: int = MAX_CAPACITY
-        self.packages: list["Package"] = []
+        self.packages: list[Package] = []
         self.avg_speed: int = AVG_SPEED  # MPH
         self.distance_traveled: float = 0.0
         self.current_address: str = "HUB"
-        self.delivery_graph: Optional["Graph"] = None
+        self.delivery_graph: Optional[Graph] = None
 
     def __repr__(self) -> str:
         return f"Truck ID: {self.truck_id}, Current Driver: {self.driver.driver_id}, Packages Loaded: {self.packages}"
@@ -33,38 +33,33 @@ class Truck:
     def get_total_packages(self) -> int:
         return len(self.packages)
 
-    def determine_path(self, src: str, dest: str) -> list["Node"]:
-        for node in self.delivery_graph.nodes:
-            node.cost = sys.maxsize
-            node.prev_node = []
-
+    def determine_path(self, src: str, dest: str):
         self.delivery_graph.get_node_by_address(src).cost = 0
-        visited: list["Node"] = []
-        curr_node: "Node" = self.delivery_graph.get_node_by_address(src)
+        min_queue: heapq = []
+        path: list["Edge"] = []
+        heapq.heapify(min_queue)
 
-        for i in range(len(self.delivery_graph.nodes) - 1):
-            if curr_node not in visited:
-                visited.append(curr_node)
-                min_heap: heapq = []
-                edges: list["Edge"] = self.delivery_graph.get_node_by_address(curr_node.node_address).edges
-                for edge in edges:
-                    if edge.node_2 not in visited:
-                        cost = curr_node.cost + edge.node_2.cost
-                        if cost < edge.node_2.cost:
-                            edge.node_2.cost = cost
-                            edge.node_2.prev_node = curr_node.prev_node + [curr_node]
-                        heapq.heappush(min_heap, [edge.node_1.cost, edge.node_2])
-                heapq.heapify(min_heap)
-                curr_node = min_heap[0][1]
+        for node in self.delivery_graph.nodes:
+            heapq.heappush(min_queue, node)
 
-        return self.delivery_graph.get_node_by_address(dest).prev_node + [self.delivery_graph.get_node_by_address(dest)]
+        while len(min_queue) != 0:
+            min_node: Node = heapq.heappop(min_queue)
+            for edge in min_node.edges:
+                alt: float = edge.priority
+                if alt < edge.node_1.cost and edge.node_1 in self.delivery_graph.nodes:
+                    self.delivery_graph.get_node_by_address(edge.node_1.node_address).cost = alt
+                    self.delivery_graph.get_node_by_address(edge.node_1.node_address).prev_node.append(min_node)
+                    heapq.heappush(min_queue, edge.node_1)
+
+        print(self.delivery_graph.get_node_by_address(dest).prev_node)
+        return self.delivery_graph.get_node_by_address(dest).prev_node.append(self.delivery_graph.get_node_by_address(dest))
 
     def go_to_next_node(self, next_node_address: str) -> None:
         self.current_address = next_node_address
         self.deliver_package()
 
     def deliver_package(self) -> list["Package"]:
-        matching_packages: list["Package"] = []
+        matching_packages: list[Package] = []
         for index, package in enumerate(self.packages):
             if package.address == self.current_address:
                 matching_packages.append(self.packages.pop(index))
