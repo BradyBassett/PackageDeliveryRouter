0a9fc0a74a97148ac342b51eb05f6cb324eb5f1f
BradyBassett <bradydbassett@gmail.com>
2022-04-18 21:35:28 -0500

    renamed node references to origin and destination

--- src/models/edge.py
+++ src/models/edge.py
@@ -1,35 +1,35 @@
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from node import Node
     from package import Package
 
 
 class Edge:
-    def __init__(self, node_1: "Node", node_2: "Node", distance: float) -> None:
-        self.node_1: Node = node_1
-        node_1.add_edge(self)
-        self.node_2: Node = node_2
-        node_2.add_edge(self)
+    def __init__(self, origin: "Node", destination: "Node", distance: float) -> None:
+        self.destination: Node = destination
+        destination.add_edge(self)
+        self.origin: Node = origin
+        origin.add_edge(self)
         self.distance: float = distance
         self.priority: float = 0
 
     def __repr__(self) -> str:
-        return f"{self.distance} miles between {self.node_1.node_name} and {self.node_2.node_name}"
+        return f"{self.origin.node_name} -> {self.distance} -> {self.destination.node_name}"
 
     def __hash__(self):
         return id(self)
 
     def __eq__(self, other: "Edge"):
-        return self.node_1.node_id == other.node_1.node_id and self.node_2.node_id == other.node_2.node_id
+        return self.destination.node_id == other.destination.node_id and self.origin.node_id == other.origin.node_id
 
     def eligible(self, nodes: list["Node"]) -> bool:
         # Intended to check a group of nodes and see if any of them are present within the current edge to filter
         # out all nodes that aren't needed to be checked for the current trucks route
-        return bool(set(nodes) & {self.node_1, self.node_2})
+        return bool(set(nodes) & {self.destination, self.origin})
 
     def calculate_priority(self, packages: list["Package"]) -> None:
         priority: float = self.distance
         for package in packages:
-            if package.address == self.node_2.node_address:
+            if package.address == self.origin.node_address:
                 priority += package.priority
         self.priority += priority
