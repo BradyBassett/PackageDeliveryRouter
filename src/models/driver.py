d76544d8468852580300e8fde7a8b8e3e80d9705
BradyBassett <bradydbassett@gmail.com>
2022-04-25 18:11:23 -0500

    doesn't select a truck if it is returned

--- src/models/driver.py
+++ src/models/driver.py
@@ -12,15 +12,14 @@ class Driver:
     def __init__(self, driver_id: int) -> None:
         self.driver_id: int = driver_id
         self.current_truck: Optional[Truck] = None
-        self.current_time: datetime = datetime(datetime.now().year, datetime.now().month, datetime.now().day,
-                                               START_HOUR)
+        self.current_time: datetime = datetime.now().replace(hour=START_HOUR, minute=0, second=0, microsecond=0)
 
     def __repr__(self) -> str:
         return f"Driver ID: {self.driver_id}, Current Truck: {self.current_truck.truck_id}"
 
     def select_truck(self, trucks: list["Truck"]):
         for truck in trucks:
-            if truck.driver is None:
+            if truck.driver is None and truck.returned is False:
                 self.current_truck = truck
                 truck.driver = self
                 break
