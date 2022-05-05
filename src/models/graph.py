a93ccd3df18d1c94773ea5cf3a6e91cce4012d0f
BradyBassett <bradydbassett@gmail.com>
2022-05-05 13:11:29 -0500

    added node and edges lists

--- src/models/graph.py
+++ src/models/graph.py
@@ -1,36 +1,39 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 from .hash_table import HashTable
+
 if TYPE_CHECKING:
     from node import Node
     from edge import Edge
 
 
-def get_closest_node(node: "Node") -> "Node":
-    edges: list[Edge] = node.edges
-    edges.sort(key=lambda edge: edge.distance)
-    return edges[0].origin if node == edges[0].destination else edges[0].destination
-
-
 class Graph:
-    def __init__(self, nodes=None, edges=None) -> None:
+    def __init__(self, nodes=None, edges=None, nodes_list=None, edges_list=None) -> None:
         if nodes is None:
             nodes: HashTable = HashTable()
+        if nodes_list is None:
+            nodes_list: list["Node"] = []
         if edges is None:
             edges: HashTable = HashTable()
+        if edges_list is None:
+            edges_list: list["Edge"] = []
 
         self.nodes: HashTable = nodes
+        self.nodes_list: list["Node"] = nodes_list
         self.edges: HashTable = edges
+        self.edges_list: list["Edge"] = edges_list
 
     def add_node(self, node: "Node") -> None:
         self.nodes.insert(node.node_address, node)
+        self.nodes_list.append(node)
 
     def add_edge(self, edge: "Edge") -> None:
         self.edges.insert((edge.origin.node_address, edge.destination.node_address), edge)
+        self.edges_list.append(edge)
 
-    def lookup_node(self, address: str):
+    def lookup_node(self, address: str) -> Optional["Node"]:
         return self.nodes.lookup(address)
 
-    def lookup_edge(self, origin_address: str, destination_address: str):
+    def lookup_edge(self, origin_address: str, destination_address: str) -> Optional["Edge"]:
         if self.edges.lookup((origin_address, destination_address)):
             return self.edges.lookup((origin_address, destination_address))
         else:
